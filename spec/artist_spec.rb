deb95535c458a2df2f0493da3dc041df8a2f7256
Jasmine Carter <jasmine.carter87@gmail.com>
2019-07-26 23:01:09 +0000

    Automatically backed up by Learn

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe "Artist" do
   it "has a name" do
-    artist = Artist.new("Beyonce")
+    artist = Artist.new
     artist.name = "Beyonce"
     expect(artist.name).to eq("Beyonce")
   end
